1ee341fbc366d37729ade37a7858c7b5d59eee07
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-03 20:42:32 +0500

    feat: created Close Button by using useState

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ const messages = [
 // we use a component called App as the root component of the application
 // it's like a parent component of all components
 export default function App() {
-  // says in which step we are
-  // const step = 1;
-
   // 1. create a state variable
   // 2. use the state in JSX
   // 3. update the state in the event handling (example: handlePrevious & handleNext)
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   // inside of component, we can define any functions we want
   const handlePrevious = function () {
@@ -32,34 +30,42 @@ export default function App() {
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
 
-      <p className="message">
-        Step {step} : {messages[step - 1]}
-      </p>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      {/* onEvent={() => ...} OR onEvent={function} is handling events in React way */}
-      {/* in () it's not a function call, it IS a really function */}
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious} // we just passing a value into the function
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <p className="message">
+            Step {step} : {messages[step - 1]}
+          </p>
+
+          {/* onEvent={() => ...} OR onEvent={function} is handling events in React way */}
+          {/* in () it's not a function call, it IS a really function */}
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious} // we just passing a value into the function
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
@@ -70,6 +76,6 @@ export default function App() {
 // -> returns an array
 // -> const [state variable (1st value), setNameOfStateVariable] = useState(argument)
 // setNameOfStateVariable is a function for updating the state variable
-// useState is ony allowed to call on the top-level of the function
+// -> useState is ony allowed to call on the top-level of the function
 
 // 2. any function in React that starts from 'use' is called 'hook'
