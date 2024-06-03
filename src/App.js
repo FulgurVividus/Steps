38e619e9cb73a9fce7ee193e3d793f11937e6c85
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-03 14:44:07 +0500

    feat: created a new component from scratch

---
+++ src/App.js
@@ -0,0 +1,37 @@
+// we use a component called App as the root component of the application
+// it's like a parent component of all components
+
+// this data doesn't depend on anything inside of the component, so placed it outside
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  // says in which step we are
+  const step = 1;
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step} : {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
